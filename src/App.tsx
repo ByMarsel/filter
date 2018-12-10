acdcbb6c3e5b5fa240c08222ae28a5c8cfc4bcec
Marsel <chumachechii>
2018-12-10 09:22:02 +0500

    заливаю проект

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Catalog from "containers/Catalog";
+
+class App extends React.Component {
+  public render() {
+    return <Catalog />;
+  }
+}
+
+export default App;
